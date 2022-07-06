94fadd1f1c57a8ca35dcff58aa6cf6ec13f43473
Pampattitude <pampattitude@gmail.com>
2022-07-06 14:22:58 +0200

    removed build

--- build/engine/config.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.defaultRules = void 0;
-exports.defaultRules = {
-    solitude: 1,
-    overpopulation: 4,
-    emptySpace: 3,
-};
